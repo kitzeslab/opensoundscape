eb71183a58c65886f04797cae8c528b078c073f1
Barry Moore <moore0557@gmail.com>
2019-08-21 16:12:15 -0400

    Use classmethod to deal with reading audio

--- opensoundscape/spectrogram.py
+++ opensoundscape/spectrogram.py
@@ -13,24 +13,57 @@ class Spectrogram:
 
     __slots__ = ("frequencies", "times", "spectrogram")
 
-    def __init__(
-        self, samples, window="hann", overlap=75, segment_length=512
-    ):
-        if not isinstance(samples, Samples):
-            raise TypeError(f"Spectrogram expects a Samples class as input, got: '{samples.__class__}'")
+    def __init__(self, spectrogram, frequencies, times):
+        if not isinstance(spectrogram, np.ndarray):
+            raise TypeError(
+                f"Spectrogram.spectrogram should be a np.ndarray [shape=(n, m)]. Got {spectrogram.__class__}"
+            )
+        if not isinstance(frequencies, np.ndarray):
+            raise TypeError(
+                f"Spectrogram.frequencies should be an np.ndarray [shape=(n,)]. Got {frequencies.__class__}"
+            )
+        if not isinstance(times, np.ndarray):
+            raise TypeError(
+                f"Spectrogram.times should be an np.ndarray [shape=(m,)]. Got {times.__class__}"
+            )
+
+        if spectrogram.ndim != 2:
+            raise TypeError(
+                f"spectrogram should be a np.ndarray [shape=(n, m)]. Got {spectrogram.shape}"
+            )
+        if frequencies.ndim != 1:
+            raise TypeError(
+                f"frequencies should be an np.ndarray [shape=(n,)]. Got {frequencies.shape}"
+            )
+        if times.ndim != 1:
+            raise TypeError(
+                f"times should be an np.ndarray [shape=(m,)]. Got {times.shape}"
+            )
+
+        if spectrogram.shape != (frequencies.shape[0], times.shape[0]):
+            raise TypeError(
+                f"Dimension mismatch, spectrogram.shape: {spectrogram.shape}, frequencies.shape: {frequencies.shape}, times.shape: {times.shape}"
+            )
+
+        super(Spectrogram, self).__setattr__("frequencies", frequencies)
+        super(Spectrogram, self).__setattr__("times", times)
+        super(Spectrogram, self).__setattr__("spectrogram", spectrogram)
+
+    @classmethod
+    def from_audio(cls, audio, window="hann", overlap=75, segment_length=512):
+        if not isinstance(audio, Samples):
+            raise TypeError("Class method expects Samples class as input")
 
         frequencies, times, spectrogram = signal.spectrogram(
-            samples.samples,
-            samples.sample_rate,
+            audio.samples,
+            audio.sample_rate,
             window=window,
             nperseg=segment_length,
             noverlap=segment_length * overlap / 100,
             scaling="spectrum",
         )
 
-        super(Spectrogram, self).__setattr__("frequencies", frequencies)
-        super(Spectrogram, self).__setattr__("times", times)
-        super(Spectrogram, self).__setattr__("spectrogram", spectrogram)
+        return cls(spectrogram, frequencies, times)
 
     def __setattr__(self, name, value):
         raise AttributeError("Spectrogram's cannot be modified")
@@ -46,4 +79,6 @@ class Spectrogram:
         remove_zeros[remove_zeros == 0.0] = np.nan
         in_decibel = 10.0 * np.log10(remove_zeros)
         in_decibel[in_decibel <= decibel_threshold] = decibel_threshold
-        return np.nan_to_num(10.0 ** (in_decibel / 10.0)), self.frequencies, self.times
+        return Spectrogram(
+            np.nan_to_num(10.0 ** (in_decibel / 10.0)), self.frequencies, self.times
+        )
--- tests/test_spectrogram.py
+++ tests/test_spectrogram.py
@@ -1,19 +1,67 @@
 #!/usr/bin/env python3
 import opensoundscape as opso
 from opensoundscape.audio import load_samples
-from opensoundscape.spectrogram import *
+from opensoundscape.spectrogram import Spectrogram
 import pytest
+import numpy as np
+
 
 @pytest.fixture()
 def veryshort_wav_str():
     return f"tests/veryshort.wav"
 
+
 def test_spectrogram_raises_typeerror():
     with pytest.raises(TypeError):
-        Spectrogram("not samples")
+        Spectrogram.from_audio("not samples")
+
 
 def test_spectrogram_shape_of_veryshort(veryshort_wav_str):
-    spec = Spectrogram(load_samples(veryshort_wav_str))
+    spec = Spectrogram.from_audio(load_samples(veryshort_wav_str))
     assert spec.spectrogram.shape == (257, 21)
     assert spec.frequencies.shape == (257,)
     assert spec.times.shape == (21,)
+
+
+def test_construct_spectrogram_spectrogram_str_raises():
+    with pytest.raises(TypeError):
+        Spectrogram("raises", np.zeros((5)), np.zeros((10)))
+
+
+def test_construct_spectrogram_frequencies_str_raises():
+    with pytest.raises(TypeError):
+        Spectrogram(np.zeros((5, 10)), "raises", np.zeros((10)))
+
+
+def test_construct_spectrogram_times_str_raises():
+    with pytest.raises(TypeError):
+        Spectrogram(np.zeros((5, 10)), np.zeros((5)), "raises")
+
+
+def test_construct_spectrogram_spectrogram_1d_raises():
+    with pytest.raises(TypeError):
+        Spectrogram(np.zeros((5)), np.zeros((5)), np.zeros((5)))
+
+
+def test_construct_spectrogram_frequencies_2d_raises():
+    with pytest.raises(TypeError):
+        Spectrogram(np.zeros((5, 5)), np.zeros((5, 5)), np.zeros((5)))
+
+
+def test_construct_spectrogram_times_2d_raises():
+    with pytest.raises(TypeError):
+        Spectrogram(np.zeros((5, 5)), np.zeros((5)), np.zeros((5, 5)))
+
+
+def test_construct_spectrogram_dimensions_mismatch_raises_one():
+    with pytest.raises(TypeError):
+        Spectrogram(np.zeros((5, 10)), np.zeros((5)), np.zeros((7)))
+
+
+def test_construct_spectrogram_dimensions_mismatch_raises_two():
+    with pytest.raises(TypeError):
+        Spectrogram(np.zeros((5, 10)), np.zeros((3)), np.zeros((10)))
+
+
+def test_construct_spectrogram():
+    Spectrogram(np.zeros((5, 10)), np.zeros((5)), np.zeros((10)))
