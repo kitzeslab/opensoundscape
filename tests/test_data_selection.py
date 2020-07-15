cfb913fa948db92da52c6e6f981bff3e18228bf2
Barry Moore <chiroptical@gmail.com>
2020-07-15 17:47:16 -0400

    Implement class balanced upsampling for DataFrames

--- tests/test_data_selection.py
+++ tests/test_data_selection.py
@@ -4,6 +4,11 @@ import opensoundscape.data_selection as selection
 import pandas as pd
 
 
+@pytest.fixture
+def upsample_df():
+    return pd.read_csv("tests/to_upsample.csv")
+
+
 def test_expand_multi_labeled_dataframe():
     input_df = pd.DataFrame({"Labels": ["hello|world"]})
     output_df = pd.DataFrame({"Labels": ["hello", "world"]})
@@ -35,3 +40,9 @@ def test_train_valid_binary_split():
     assert valid_df[valid_df["Labels"] == "hello"]["Labels"].count() == 1
     assert train_df[train_df["Labels"] == "world"]["Labels"].count() == 4
     assert valid_df[valid_df["Labels"] == "world"]["Labels"].count() == 1
+
+
+def test_upsample_basic(upsample_df):
+    for _ in range(100):
+        upsampled_df = selection.upsample(upsample_df)
+        assert upsampled_df.shape[0] == 20
