Implement correct scaling procedure

opensoundscape/datasets.py
@@ -248,7 +248,7 @@ class BinaryFromAudio(torch.utils.data.Dataset):
         audio_p = Path(row[self.audio_column])
         audio = Audio.from_file(audio_p)
         spectrogram = Spectrogram.from_audio(audio)
-        spectrogram = spectrogram.min_max_scale(feature_range=(0, 255))
+        spectrogram = spectrogram.linear_scale(feature_range=(0, 255))
 
         image = Image.fromarray(spectrogram.spectrogram)
         image = image.convert("RGB")

opensoundscape/spectrogram.py
@@ -5,7 +5,7 @@
 from scipy import signal
 import numpy as np
 from opensoundscape.audio import Audio
-from opensoundscape.helpers import min_max_scale
+from opensoundscape.helpers import min_max_scale, linear_scale
 import warnings
 import pickle
 
@@ -14,11 +14,7 @@ class Spectrogram:
     """ Immutable spectrogram container
     """
 
-    __slots__ = (
-        "frequencies",
-        "times",
-        "spectrogram",
-    )  # , "overlap", "segment_length")
+    __slots__ = ("frequencies", "times", "spectrogram", "decibel_limits")
 
     def __init__(self, spectrogram, frequencies, times):
         if not isinstance(spectrogram, np.ndarray):
@@ -55,6 +51,7 @@ class Spectrogram:
         super(Spectrogram, self).__setattr__("frequencies", frequencies)
         super(Spectrogram, self).__setattr__("times", times)
         super(Spectrogram, self).__setattr__("spectrogram", spectrogram)
+        super(Spectrogram, self).__setattr__("decibel_limits", (-100, -20))
 
     @classmethod
     def from_audio(
@@ -101,7 +98,9 @@ class Spectrogram:
         spectrogram[spectrogram > max_db] = max_db
         spectrogram[spectrogram < min_db] = min_db
 
-        return cls(spectrogram, frequencies, times)
+        new_obj = cls(spectrogram, frequencies, times)
+        super(Spectrogram, new_obj).__setattr__("decibel_limits", decibel_limits)
+        return new_obj
 
     def __setattr__(self, name, value):
         raise AttributeError("Spectrogram's cannot be modified")
@@ -110,10 +109,14 @@ class Spectrogram:
         return f"<Spectrogram(spectrogram={self.spectrogram.shape}, frequencies={self.frequencies.shape}, times={self.times.shape})>"
 
     def min_max_scale(self, feature_range=(0, 1)):
-        """ Linearly rescale spectrogram values to a range of values
+        """
+
+        Linearly rescale spectrogram values to a range of values using
+        in_range as minimum and maximum
         
         Args:
             feature_range: tuple of (low,high) values for output
+
         Returns:
             Spectrogram object with values rescaled to feature_range
         """
@@ -127,6 +130,34 @@ class Spectrogram:
 
         return Spectrogram(
             min_max_scale(self.spectrogram, feature_range=feature_range),
+            self.frequencies,
+            self.times,
+        )
+
+    def linear_scale(self, feature_range=(0, 1)):
+        """
+
+        Linearly rescale spectrogram values to a range of values
+        using in_range as decibel_limits
+        
+        Args:
+            feature_range: tuple of (low,high) values for output
+
+        Returns:
+            Spectrogram object with values rescaled to feature_range
+        """
+
+        if len(feature_range) != 2:
+            raise AttributeError(
+                "Error: `feature_range` doesn't look like a 2-element tuple?"
+            )
+        if feature_range[1] < feature_range[0]:
+            raise AttributeError("Error: `feature_range` isn't increasing?")
+
+        return Spectrogram(
+            linear_scale(
+                self.spectrogram, in_range=self.decibel_limits, out_range=feature_range
+            ),
             self.frequencies,
             self.times,
         )