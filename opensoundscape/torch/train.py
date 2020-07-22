1ea680bd155ff542b622389947df723254c17c98
rhine3 <tessa.rhinehart@gmail.com>
2020-07-22 18:30:03 -0400

    Bug fix: pass entire batch of single-channel images to tensor_augment

--- opensoundscape/torch/train.py
+++ opensoundscape/torch/train.py
@@ -79,10 +79,10 @@ def train(
             y.to(device)
             targets = y.squeeze(1)
 
-                # X is currently shape [1, 3, width, height]
-                # Take to shape [1, 1, width, height] for use with `augment`
-                X = X[0][0].unsqueeze(0).unsqueeze(0)
             if tensor_augment:
+                # X is currently shape [batch_size, 3, width, height]
+                # Take to shape [batch_size, 1, width, height] for use with `augment`
+                X = X[:,0].unsqueeze(1)
                 X = tensaug.time_warp(X.clone(), W=10)
                 X = tensaug.time_mask(X, T=50, max_masks=5)
                 X = tensaug.freq_mask(X, F=50, max_masks=5)
