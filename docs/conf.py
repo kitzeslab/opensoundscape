2a202814324b8fc52885dfed1faab8c1375a42e7
Tessa Rhinehart <tessa.rhinehart@gmail.com>
2021-02-25 09:17:08 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -79,7 +79,7 @@ html_theme = "sphinx_rtd_theme"
 # html_static_path = ["_static"]
 html_static_path = ['_static']
 html_css_files = [
-    'theme.css',
+    'css/greentheme.css',
 ]
 
 style_nav_header_background = '#2980B9'
