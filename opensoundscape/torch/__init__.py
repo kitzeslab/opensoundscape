14a6353c5fc488ea56756e09e28391e389f28e96
Sam Lapp <sammlapp@gmail.com>
2021-12-16 16:50:39 -0500

    update init files to allow top-level import of opensoundscape
    This enable the syntax:
    ```
    import opensoundscape as opso
    opso.audio.Audio(...)
    ```
    which would fail before. 
    We will need to update init files any time files change names/locations 
    or are added/deleted

---
+++ opensoundscape/torch/__init__.py
@@ -0,0 +1,6 @@
+from . import grad_cam
+from . import loss
+from . import safe_dataset
+from . import sampling
+from . import architectures
+from . import models
---
+++ opensoundscape/torch/models/__init__.py
@@ -0,0 +1,2 @@
+from . import cnn
+from . import utils
