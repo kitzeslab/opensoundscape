d74051b750737a359d28e82b42103574e85c2b49
Sam Lapp <sammlapp@gmail.com>
2021-12-17 15:05:47 -0500

    update version numbers to 0.6.1 everywhere

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ copyright = "2020, Justin Kitzes, Barry Moore, Tessa Rhinehart, Sam Lapp"
 author = "Justin Kitzes, Barry Moore, Tessa Rhinehart, Sam Lapp"
 
 # The full version, including alpha/beta/rc tags
-release = "0.6.0"
+release = "0.6.1"
 
 
 # -- General configuration ---------------------------------------------------
