4980699b0f64c9543e55e9de685d15d08f833f29
Sam Lapp <sammlapp@gmail.com>
2022-05-19 17:10:41 -0400

    add basic tests for metrics
    we should write more. also note that metrics for binary model may be 
    misleading (averaged over positive/negative class)

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python3
 import pytest
 import numpy as np
+
+from opensoundscape import metrics
+
+# metrics are currently implicitly tested through test_cnn
+# however, we should add explicit tests
+def test_multitarget_metrics():
+    x = [[0, 1], [1, 0]]
+    y = [[0, 1], [1, 1]]
+    out = metrics.multi_target_metrics(y, x, [0, 1], 0.5)
+    assert out["precision"] == 1
+    assert out["recall"] == 0.75
+
+
+def test_singletarget_metrics():
+    x = [[0, 1], [1, 0], [1, 0]]
+    y = [[0, 1], [0, 1], [1, 0]]
+    out = metrics.single_target_metrics(y, x, [0, 1])
+    assert out["precision"] == 1
+    assert out["recall"] == 0.5
