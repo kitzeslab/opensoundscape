a928449188e689696a8bc0a67cdd0af01a2a3d87
sammlapp <sammlapp@gmail.com>
2023-01-11 13:24:38 -0500

    Change default args from lists to tuple
    Default arg as a (mutable) list can cause unexpected/surprising behavior because the list is only initialized once and can be modified. I've changed any default arguments that were lists to tuples. Behavior is the same, but note that a 1-element tuple must be written with a comma, eg (0,) because (0) is not interpreted as a tuple

--- opensoundscape/preprocess/actions.py
+++ opensoundscape/preprocess/actions.py
@@ -76,7 +76,7 @@ class Action(BaseAction):
     Other arguments are an arbitrary list of kwargs.
     """
 
-    def __init__(self, fn, is_augmentation=False, extra_args=[], **kwargs):
+    def __init__(self, fn, is_augmentation=False, extra_args=(), **kwargs):
         super(Action, self).__init__()
 
         self.action_fn = fn
@@ -163,7 +163,7 @@ class AudioTrim(Action):
 
     def __init__(self, **kwargs):
         super(AudioTrim, self).__init__(
-            trim_audio, extra_args=["_sample_duration"], **kwargs
+            trim_audio, extra_args=("_sample_duration",), **kwargs
         )
 
 
@@ -447,7 +447,7 @@ class Overlay(Action):
         super(Overlay, self).__init__(
             overlay,
             is_augmentation=is_augmentation,
-            extra_args=["_labels", "_preprocessor"],
+            extra_args=("_labels", "_preprocessor"),
             **kwargs,
         )
 
