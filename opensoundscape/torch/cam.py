8ae716a30b62a2a35dce6cca936272aa38595297
Louis Freeland-Haynes <66101835+louisfh@users.noreply.github.com>
2023-02-20 16:58:11 -0500

    Working CAM class as container for activation maps

--- opensoundscape/torch/cam.py
+++ opensoundscape/torch/cam.py
@@ -1,27 +1,33 @@
 """ Class activation maps (CAM) for OpenSoundscape models"""
+import matplotlib.pyplot as plt
 
 
-class cam:
+class ActivationMap:
     """Base class for activation maps
     This is a container for the activation map of a given class for a given input.
     """
 
-    def __init__(self, base_image, activation_map, class_name, class_index):
+    def __init__(self, base_image, activation_map, class_name):
         self.base_image = base_image
         self.activation_map = activation_map
+        self.activation_map = self.activation_map.squeeze(0)
         self.class_name = class_name
-        self.class_index = class_index
 
     def plot(self, alpha=0.5, cmap="jet", interpolation="bilinear", figsize=(10, 10)):
         """Plot the activation map
         Args:
-            alpha: opacity of the overlay
+            alpha: opacity of the activation map overlap
             cmap: the colormap for the activation map
             interpolation: the interpolation method for the activation map
             figsize: the figure size for the plot
         """
         fig, ax = plt.subplots(figsize=figsize)
-        ax.imshow(self.base_image, alpha=alpha)
+
+        # remove the first (batch) dimension
+        # move the first dimension (Nchannels) to last dimension for imshow
+        # make negative
+        base_image = -self.base_image.squeeze(0).permute(1, 2, 0)
+        ax.imshow(base_image, alpha=1)
         ax.imshow(
             self.activation_map, cmap=cmap, alpha=alpha, interpolation=interpolation
         )
@@ -45,4 +51,4 @@ class cam:
         plt.close(fig)
 
     def __repr__(self):
-        return f"activation_map({self.base_image}, {self.activation_map}, {self.class_name}, {self.class_index})"
+        return f"ActivationMap({self.class_name})"
