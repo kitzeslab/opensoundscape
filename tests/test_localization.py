931f189ee800494f468e05009f018e993ccbccaa
Louis Freeland-Haynes <66101835+louisfh@users.noreply.github.com>
2023-06-09 13:20:57 -0400

    change 'position' to 'location' in localization

--- tests/test_localization.py
+++ tests/test_localization.py
@@ -58,20 +58,20 @@ def test_travel_time():
 
 
 def test_soundfinder_localize_2d():
-    reciever_positions = [[0, 0], [0, 20], [20, 20], [20, 0]]
+    reciever_locations = [[0, 0], [0, 20], [20, 20], [20, 0]]
     arrival_times = [1, 1, 1, 1]
     estimate = localization.soundfinder_localize(
-        reciever_positions,
+        reciever_locations,
         arrival_times,
     )
     assert close(np.linalg.norm(np.array(estimate[0:2]) - np.array([10, 10])), 0, 0.01)
 
 
 def test_soundfinder_3d():
-    reciever_positions = [[0, 0, 0], [0, 20, 1], [20, 20, -1], [20, 0, 0.1]]
+    reciever_locations = [[0, 0, 0], [0, 20, 1], [20, 20, -1], [20, 0, 0.1]]
     arrival_times = [1, 1, 1, 1]
     estimate = localization.soundfinder_localize(
-        reciever_positions,
+        reciever_locations,
         arrival_times,
     )
     assert close(
@@ -81,11 +81,11 @@ def test_soundfinder_3d():
 
 def test_soundfinder_lstsq():
     # currently not implemented
-    reciever_positions = [[0, 0, 0], [0, 20, 1], [20, 20, -1], [20, 0, 0.1]]
+    reciever_locations = [[0, 0, 0], [0, 20, 1], [20, 20, -1], [20, 0, 0.1]]
     arrival_times = [1, 1, 1, 1]
     with pytest.raises(NotImplementedError):
         estimate = localization.soundfinder_localize(
-            reciever_positions, arrival_times, invert_alg="lstsq"
+            reciever_locations, arrival_times, invert_alg="lstsq"
         )
     # assert close(
     #     np.linalg.norm(np.array(estimate[0:3]) - np.array([10, 10, 0])), 0, 0.1
@@ -93,10 +93,10 @@ def test_soundfinder_lstsq():
 
 
 def test_soundfinder_nocenter():
-    reciever_positions = [[100, 0, 0], [100, 20, 1], [120, 20, -1], [120, 0, 0.1]]
+    reciever_locations = [[100, 0, 0], [100, 20, 1], [120, 20, -1], [120, 0, 0.1]]
     arrival_times = [1, 1, 1, 1]
     estimate = localization.soundfinder_localize(
-        reciever_positions,
+        reciever_locations,
         arrival_times,
         center=False,  # True for original Sound Finder behavior
     )
@@ -106,53 +106,53 @@ def test_soundfinder_nocenter():
 
 
 def test_gillette_localize_raises():
-    reciever_positions = [[100, 0], [100, 20], [120, 20], [120, 0]]
+    reciever_locations = [[100, 0], [100, 20], [120, 20], [120, 0]]
     arrival_times = [1, 1, 1, 1]
 
     # check this raises a ValueError because none of the arrival times are zero
     with pytest.raises(ValueError):
-        localization.gillette_localize(reciever_positions, arrival_times)
+        localization.gillette_localize(reciever_locations, arrival_times)
 
 
 def test_gillette_localize_2d():
     np.random.seed(0)
-    receiver_positions = np.array([[0, 0], [0, 20], [20, 20], [20, 0], [10, 10]])
+    receiver_locations = np.array([[0, 0], [0, 20], [20, 20], [20, 0], [10, 10]])
     sound_source = np.random.rand(2) * 20
     speed_of_sound = 343
     time_of_flight = (
-        np.linalg.norm(receiver_positions - sound_source, axis=1) / speed_of_sound
+        np.linalg.norm(receiver_locations - sound_source, axis=1) / speed_of_sound
     )
     tdoas = time_of_flight - np.min(time_of_flight)
 
-    estimated_pos = localization.gillette_localize(receiver_positions, tdoas)
+    estimated_pos = localization.gillette_localize(receiver_locations, tdoas)
 
     assert np.allclose(estimated_pos, sound_source, rtol=0.1)
 
 
 def test_gillette_localize_3d():
-    receiver_positions = np.array(
+    receiver_locations = np.array(
         [[0, 0, 10], [0, 20, 1], [20, 20, -1], [20, 0, 0.1], [10, 10, 0], [5, 5, 5]]
     )
     sound_source = np.array([10, 12, 2])
     speed_of_sound = 343
     time_of_flight = (
-        np.linalg.norm(receiver_positions - sound_source, axis=1) / speed_of_sound
+        np.linalg.norm(receiver_locations - sound_source, axis=1) / speed_of_sound
     )
 
     # localize with each receiver as reference:
     for ref_index in range(len(time_of_flight)):
         tdoas = time_of_flight - time_of_flight[ref_index]
 
-        estimated_pos = localization.gillette_localize(receiver_positions, tdoas)
+        estimated_pos = localization.gillette_localize(receiver_locations, tdoas)
 
         assert np.allclose(estimated_pos, sound_source, atol=2.5)
 
 
 def test_soundfinder_nopseudo():
-    reciever_positions = [[0, 0, 0], [0, 20, 1], [20, 20, -1], [20, 0, 0.1]]
+    reciever_locations = [[0, 0, 0], [0, 20, 1], [20, 20, -1], [20, 0, 0.1]]
     arrival_times = [1, 1, 1, 1]
     estimate = localization.soundfinder_localize(
-        reciever_positions,
+        reciever_locations,
         arrival_times,
         invert_alg="gps",  # options: 'lstsq', 'gps'
         center=True,  # True for original Sound Finder behavior
@@ -173,14 +173,14 @@ def test_localization_pipeline(file_coords_csv, predictions_csv):
         max_receiver_dist=100,
         localization_algorithm="gillette",
     )
-    # the audio files were generated according to the "true" event position:
+    # the audio files were generated according to the "true" event location:
     true_x = 10
     true_y = 15
     assert len(localized_events) == 5
 
     for event in localized_events:
-        assert math.isclose(event.position_estimate[0], true_x, abs_tol=2)
-        assert math.isclose(event.position_estimate[1], true_y, abs_tol=2)
+        assert math.isclose(event.location_estimate[0], true_x, abs_tol=2)
+        assert math.isclose(event.location_estimate[1], true_y, abs_tol=2)
 
 
 def test_localization_pipeline_real_audio(LOCA_2021_aru_coords, LOCA_2021_detections):
