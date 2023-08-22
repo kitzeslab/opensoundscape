2a60ba575567afe5b2f4691aede550e1134285b9
sammlapp <sammlapp@gmail.com>
2023-08-21 21:55:24 -0700

    add tests for cam

--- tests/test_cam.py
+++ tests/test_cam.py
@@ -1,2 +1,34 @@
 # test generating cam
 # test plotting and saving
+
+# unit tests for CAM class
+import pytest
+import torch
+import numpy as np
+from opensoundscape.ml.cam import CAM
+import pandas as pd
+
+
+@pytest.fixture()
+def cam():
+    base = torch.rand(3, 224, 224)
+    activation_maps = pd.Series({i: torch.rand(224, 224) for i in range(2)})
+    gbp_maps = pd.Series({i: torch.rand(224, 224, 3) for i in range(2)})
+    return CAM(
+        base_image=base,
+        activation_maps=activation_maps,
+        gbp_maps=gbp_maps,
+    )
+
+
+def test_cam_init(cam):
+    assert isinstance(cam, CAM)
+    assert isinstance(cam.base_image, torch.Tensor)
+    assert isinstance(cam.activation_maps, pd.Series)
+    assert isinstance(cam.gbp_maps, pd.Series)
+
+
+def test_cam_plot(cam):
+    cam.plot(target_class=0)
+    cam.plot(target_class=0, mode="backprop")
+    cam.plot(target_class=0, mode="backprop_and_activation")
